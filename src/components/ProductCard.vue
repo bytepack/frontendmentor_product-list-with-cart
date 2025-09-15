bd4922f06261c790befedf941139cac4fb56cbec
bytepack <bytepack3@proton.me>
2025-09-15 10:32:51 +0330

    border animation for card

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed } from 'vue'
+import { computed, ref, watch } from 'vue'
 import IconIncrement from '@/components/icons/IconIncrementQuantity.vue'
 import IconDecrement from '@/components/icons/IconDecrementQuantity.vue'
 import { useCartStore } from '@/stores/cart'
@@ -13,43 +13,53 @@ const { product } = defineProps({
 
 const cartStore = useCartStore()
 
-const quantityInCart = computed(() => {
+const quantity = computed(() => {
   const item = cartStore.cartItems.find((item) => item.name === product.name)
   return item ? item.quantity : 0
+})
+
+const quantityChanged = ref(false)
+watch(quantity, () => {
+  quantityChanged.value = true
 })
 
 function handleAddToCart() {
   cartStore.addToCart(product)
 }
-function incrementQuantityInCart() {
+function incrementQuantity() {
   cartStore.updateQuantity(product.name, 'INC')
 }
 
-function decrementQuantityInCart() {
+function decrementQuantity() {
   cartStore.updateQuantity(product.name, 'DEC')
 }
 </script>
+
 <template>
   <div class="flex flex-col gap-0.5">
-    <picture>
-      <source media="(min-width: 64rem)" :srcset="product.image.desktop" />
-      <source media="(min-width: 40rem)" :srcset="product.image.tablet" />
-      <img
-        :src="product.image.mobile"
-        width="654"
-        height="424"
-        class="border-2 rounded-lg shadow-lg aspect-[654/424] md:aspect-[427/424] lg:aspect-[502/480]"
-        :class="[quantityInCart ? 'border-orange-700' : 'border-transparent']"
-        loading="lazy"
-        :alt="product.name"
-      />
-    </picture>
     <div
-      v-if="quantityInCart"
+      class="relative flex justify-center items-center overflow-hidden p-1 rounded-lg shadow-lg before:content-[''] before:absolute before:w-[200%] before:h-[200%] before:-z-1"
+      :class="[quantity > 0 ? 'animate-card' : quantityChanged ? 'animate-card-reverse' : '']"
+    >
+      <picture>
+        <source media="(min-width: 64rem)" :srcset="product.image.desktop" />
+        <source media="(min-width: 40rem)" :srcset="product.image.tablet" />
+        <img
+          :src="product.image.mobile"
+          width="654"
+          height="424"
+          class="rounded-lg aspect-[654/424] md:aspect-[427/424] lg:aspect-[502/480]"
+          loading="lazy"
+          :alt="product.name"
+        />
+      </picture>
+    </div>
+    <div
+      v-if="quantity"
       class="flex justify-between items-center gap-8 self-center -translate-y-1/2 -mt-0.5 bg-orange-700 px-6 py-3 rounded-full border-1 border-orange-400"
     >
       <button
-        @click="decrementQuantityInCart()"
+        @click="decrementQuantity()"
         class="group p-0.5 border-2 border-white rounded-full cursor-pointer transition-colors duration-200 hover:bg-white"
         type="button"
       >
@@ -60,9 +70,9 @@ function decrementQuantityInCart() {
         />
       </button>
 
-      <span class="text-white font-semibold">{{ quantityInCart }}</span>
+      <span class="text-white font-semibold">{{ quantity }}</span>
       <button
-        @click="incrementQuantityInCart()"
+        @click="incrementQuantity()"
         class="group p-0.5 border-2 border-white rounded-full cursor-pointer transition-colors duration-200 hover:bg-white"
         type="button"
       >
@@ -88,3 +98,42 @@ function decrementQuantityInCart() {
     <div class="text-orange-700 font-semibold">${{ product.price }}</div>
   </div>
 </template>
+
+<style scoped>
+@reference "@/assets/main.css";
+
+.animate-card::before {
+  @apply animate-[spin-fill_1.5s_ease-in-out_1_forwards] bg-conic from-orange-700 to-transparent;
+}
+
+.animate-card-reverse::before {
+  @apply animate-[spin-fill-fade_1s_linear_1_forwards] bg-conic from-orange-700 to-transparent;
+}
+
+@keyframes spin-fill {
+  0% {
+    transform: rotate(0deg);
+    background-color: transparent;
+  }
+  100% {
+    transform: rotate(-360deg);
+    background-color: var(--color-orange-700);
+  }
+}
+
+@keyframes spin-fill-fade {
+  0% {
+    transform: rotate(-360deg);
+    background-color: var(--color-orange-700);
+    opacity: 1;
+  }
+  80% {
+    opacity: 1;
+  }
+  100% {
+    transform: rotate(0deg);
+    background-color: transparent;
+    opacity: 0;
+  }
+}
+</style>
